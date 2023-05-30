4e2cabb79365ed130ce595af0f9c614cf03d2830
jongsik-moon <jongsik.moon@nearthlab.com>
2023-05-30 05:17:03 +0000

    add viz_h5.py

---
+++ viz_h5.py
@@ -0,0 +1,62 @@
+#%%
+import h5py
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
+from collections import defaultdict
+
+
+def load_h5_2layer(filename):
+    '''Loads dictionary from hdf5 file'''
+
+    dict_to_load = defaultdict(dict)
+    with h5py.File(filename, 'r') as f:
+        keys = [key for key in f.keys()]
+        for key1 in keys:
+            for key2 in f[key1].keys():
+                dict_to_load[key1][key2] = f[key1][key2][()]
+    return dict_to_load
+
+
+def load_h5_3layer(filename):
+    '''Loads dictionary from hdf5 file'''
+
+    dict_to_load = defaultdict(dict)
+    with h5py.File(filename, 'r') as f:
+        keys = [key for key in f.keys()]
+        for key1 in keys:
+            dict_to_load[key1] = defaultdict(dict)
+            for key2 in f[key1].keys():
+                for key3 in f[key1][key2].keys():
+                    dict_to_load[key1][key2][key3] = f[key1][key2][key3][()]
+    return dict_to_load
+
+
+PATH_TO_FEATS = '/home/jsmoon/outputs/haiper-fountain'
+matches = load_h5_3layer(
+    os.path.join(PATH_TO_FEATS, 'matches-patch2pix_pairs-sfm.h5'))
+IMG_DIR = '/home/jsmoon/kaggle/input/image-matching-challenge-2023/train/haiper/fountain/images'
+img1_key = 'image_000.jpeg'
+img2_key = 'image_007.jpeg'
+img1 = cv2.cvtColor(cv2.imread(os.path.join(IMG_DIR, img1_key)),
+                    cv2.COLOR_BGR2RGB)
+img2 = cv2.cvtColor(cv2.imread(os.path.join(IMG_DIR, img2_key)),
+                    cv2.COLOR_BGR2RGB)
+plt.imshow(np.concatenate([img1, img2], axis=1))
+
+# %%
+import kornia.feature as KF
+import torch
+from kornia_moons.feature import *
+
+kpt0 = np.array(matches[img1_key][img2_key]['keypoints0'])
+kpt1 = np.array(matches[img1_key][img2_key]['keypoints1'])
+matches = np.array(matches[img1_key][img2_key]['matches0'])
+img_out = cv2.drawMatches(img1, kpt0, img2, kpt1, matches, None)
+plt.figure()
+fig, ax = plt.subplots(figsize=(15, 15))
+ax.imshow(img_out, interpolation='nearest')
+
+# %%
