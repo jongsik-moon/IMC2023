834013277f44a27ec7091465b51e74499846f125
jongsik-moon <jongsik.moon@nearthlab.com>
2023-05-30 05:17:52 +0000

    update viz_spsg that query image not added to ref

--- viz_spsg.py
+++ viz_spsg.py
@@ -10,37 +10,24 @@ from hloc.visualization import plot_images, read_image
 src = '/home/jsmoon/kaggle/input/image-matching-challenge-2023/train'
 dataset = 'haiper'
 scene = 'chairs'
-sfm = f'/home/jsmoon/kaggle/spsg/{dataset}_{scene}'
+sfm = f'/home/jsmoon/kaggle/sosnet/{dataset}_{scene}'
 images = Path(f'{src}/{dataset}/{scene}/images')
-outputs = Path(f'/home/jsmoon/kaggle/spsg/{dataset}_{scene}')
+outputs = Path(f'/home/jsmoon/kaggle/sosnet/{dataset}_{scene}')
 sfm_pairs = outputs / 'pairs-sfm.txt'
 loc_pairs = outputs / 'pairs-loc.txt'
 sfm_dir = outputs / 'sfm'
 features = outputs / 'features.h5'
 matches = outputs / 'matches.h5'
 
-feature_conf = extract_features.confs['superpoint_aachen']
-matcher_conf = match_features.confs['superglue']
-
 model = pycolmap.Reconstruction(f'{sfm}/sfm')
 
 references = [p.relative_to(images).as_posix() for p in (images).iterdir()]
-ref_ids = [r for r in model.images]
 queries = [img.name for _, img in model.images.items()]
-query = queries[1]
+print(queries)
+query = queries[0]
 print(query)
-
-extract_features.main(feature_conf,
-                      images,
-                      image_list=[query],
-                      feature_path=features,
-                      overwrite=False)
-pairs_from_exhaustive.main(loc_pairs, image_list=[query], ref_list=references)
-match_features.main(matcher_conf,
-                    loc_pairs,
-                    features=features,
-                    matches=matches,
-                    overwrite=False)
+ref_ids = [id for id, img in model.images.items() if img.name != query]
+print(ref_ids)
 
 camera = pycolmap.infer_camera_from_image(images / query)
 conf = {
@@ -61,5 +48,5 @@ ret, log = pose_from_cluster(localizer, query, camera, ref_ids, features,
 print(
     f'found {ret["num_inliers"]}/{len(ret["inliers"])} inlier correspondences.'
 )
-visualization.visualize_loc_from_log(images, query, log, model, top_k_db=5)
+visualization.visualize_loc_from_log(images, query, log, model, top_k_db=10)
 # %%
